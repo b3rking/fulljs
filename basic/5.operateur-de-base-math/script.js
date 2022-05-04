96eed7e4df7d2264e06d2c4f90fbb76cbd57272c
b3rking <ishimweeschylebersy@gmail.com>
2022-05-04 11:34:31 +0200

    fin lesson 5

---
+++ basic/5.operateur-de-base-math/script.js
@@ -0,0 +1,22 @@
+// j'ignore les operateur simple / * - +
+
+// unaire, binaire, opérande
+
+// un operateur est unaire si il a un seul operande
+let x = -1      // le - est unaire
+
+// un operateur est unaire si il a un seul operande
+let y = 7 - 8
+
+// % modulo
+console.log(5 % 2);     // retourne le reste de la division
+
+// expenantiation
+console.log(5 ** 2) // 5² 
+
+// concatenation
+
+let word = "first part"
+let second = " of it."
+
+console.log(word + second)
