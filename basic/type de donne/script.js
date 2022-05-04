d6143357aa2a2dec613cb3ed1450c337dfb7792d
b3rking <ishimweeschylebersy@gmail.com>
2022-05-04 09:51:55 +0200

    done type of data start alert prompt and confirm

---
+++ basic/type de donne/script.js
@@ -0,0 +1,20 @@
+// type de données existant en js
+
+// 1. number
+let age = 25
+
+// 2. Infinity
+alert(Infinity)
+
+// 3. NaN
+alert("just a string" / 3)
+
+// 4. string
+alert(`i'm ${age}`)
+
+// 5. boolean
+alert(1 > 0)
+
+// 6. null
+// 7. undefined
+// 8. objects and symbols
