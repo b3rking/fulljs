e204b595d05ef09459d8cbdd68b3f95a331ee9ac
b3rking <ishimweeschylebersy@gmail.com>
2022-05-04 10:17:33 +0200

    done lesson 3 starting lesson 4, adding better file naming

---
+++ basic/3.alert-prompt-confirm/script.js
@@ -0,0 +1,17 @@
+// alert, prompt and confirm method or functions
+
+// alert
+// alert('a text') 
+
+// prompt
+// let result = prompt('give me your age');
+
+// console.log(result);
+
+// let age = prompt('how old are you?');
+
+// if (age !== null) alert (`you're ${age}`)
+
+// confirm
+
+// let choice = confirm('are you crazy?')
