15b8b299bf6e276b2ad03902f9c6b5d33e9e6869
b3rking <ishimweeschylebersy@gmail.com>
2022-05-04 09:25:50 +0200

    done variables and starting data type

--- basic/variables.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-// les variables permet de stockez des valeurs dans la memoire pour une utilisation anterieur
-
-let message;
-
-message = 'Hello'
-
-alert(message);
---
+++ basic/variables/script.js
@@ -0,0 +1,16 @@
+// let message; // initialisation d'une variable vide
+
+// message = 'hello' // ajout de la valeur
+
+let message = 'hello' // declaration et affectation de la valuer
+
+alert(message)
+
+// plusieur variables sur une meme ligne
+
+let user = 'john', age = 25;
+
+// constant variables
+
+const white = '#fff'
+const black = '#000'
