17fe85b2145cb1106a5aba92048ecacac2b5b2e5
b3rking <ishimweeschylebersy@gmail.com>
2022-05-05 18:57:29 +0200

    lesson about loops

---
+++ basic/6.comparaison/script.js
@@ -0,0 +1,16 @@
+// comparaison en javascript
+
+// > < = >= <= 
+
+// saw it to many times
+
+// nullish operator
+
+// usage
+
+let user;
+
+user = null
+
+alert(user ?? 'hello') // will print hello if user is undefined or null 
+
---
+++ basic/7.loops/script.js
@@ -0,0 +1,14 @@
+while(condition) {
+    // code a executer
+}
+
+// pour un code d'une seule ligne les accolades sont pas requise
+
+do {
+    // code a executer
+} while(condition)
+
+for (debut, condition, etape) {
+    // code a executer
+}
+
