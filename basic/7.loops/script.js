1940f1255afcf72bddde1b7ec986aeb2d8db3ee5
b3rking <ishimweeschylebersy@gmail.com>
2022-05-06 10:02:06 +0200

    lesson 7 end, loops

--- basic/7.loops/script.js
+++ basic/7.loops/script.js
@@ -1,14 +1,56 @@
-while(condition) {
-    // code a executer
-}
+// while (1 < ) {
+//     // code a executer
+// }
 
 // pour un code d'une seule ligne les accolades sont pas requise
 
-do {
-    // code a executer
-} while(condition)
+// do {
+//     // code a executer
+// } while (true);
 
-for (debut, condition, etape) {
-    // code a executer
+// for (i = 0; i < 10; i++) {
+//     // code a executer
+// }
+
+// briser la boucle
+
+let i = 3;
+while (i < 3) {
+    // do things
+    if (i === 2) break;
+    i++;
+}
+
+// continuer jusqu'a la prochaine iteration
+for(let i = 0; i < 10; i++) {
+    // si vrai, saute le reste du corp de la fonction
+    if (i === 5) continue
+    console.log(i);
+}
+
+// / SWITCH  ------------------
+
+let val = 3
+
+switch (val) {
+    case 1:
+        alert('too small')
+        break
+    case 3:
+        alert('small')
+        break
+    case 5:
+        alert('finally, found it!')
+        break;
+    case 7:
+        alert('big')
+        break;
+    case 10:
+        alert('too big')
+        break;
+
+    default:
+        alert('unknown choice')
+        break;
 }
 
