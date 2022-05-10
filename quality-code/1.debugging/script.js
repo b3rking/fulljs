73da168520e59414d05e2d396432f23673c9b27e
b3rking <ishimweeschylebersy@gmail.com>
2022-05-10 10:51:38 +0200

    fin du travail sur le debugging

---
+++ quality-code/1.debugging/script.js
@@ -0,0 +1,13 @@
+const king = '';
+
+function sayHello() {
+    return 15 * 56
+}
+
+// style de codage
+
+// commenter son code
+
+// assert to write test
+
+// and voila all about the notes on debugging been too much theory
