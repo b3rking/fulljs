61fef617d00299f8fe56e8b4a57afcc08207a8a6
b3rking <ishimweeschylebersy@gmail.com>
2022-05-11 09:03:59 +0200

    operateur new

--- objects/script.js
+++ objects/script.js
@@ -7,17 +7,17 @@
 // let user = {}     // most used
 
 let user = {
-    name: 'john',
+    name: "john",
     age: 30,
-}
+};
 
 // get object data
 
-console.log(user.name)
+console.log(user.name);
 
 // add data to an object
 
-user.isAdmin = true
+user.isAdmin = true;
 
 // delete data from object
 
@@ -27,16 +27,16 @@ user.isAdmin = true
 
 let cafe = {
     "name of the cafe": "5/5 cafe",
-    location: "buyenzi"
-}
+    location: "buyenzi",
+};
 
 // get the multi word key value
 
-console.log(cafe['name of the cafe'])
+console.log(cafe["name of the cafe"]);
 
 // check if a key exist using "in"
 
-console.log('name of the cafe' in cafe)
+console.log("name of the cafe" in cafe);
 
 // loop through object
 
@@ -44,15 +44,17 @@ for (let usr in user) console.log(usr);
 
 // cloning an object
 
-let clone = {}
+let clone = {};
 
 for (let key in user) {
-    clone[key] = user[key]
+    clone[key] = user[key];
 }
 
-console.log('clone say ' +clone.name)
+console.log("clone say " + clone.name);
 
+//=========================
 // objecct method "this"
+//=========================
 
 // create an object
 // let person = {
@@ -74,8 +76,29 @@ let person = {
     age: 30,
     sayHi() {
         // this is the current object
-        console.log(`${this.name} wish you a good day!`)
-    }
+        console.log(`${this.name} wish you a good day!`);
+    },
+};
+
+person.sayHi();
+
+//=========================
+//  construnctor the "new" operator
+//=========================
+
+// the construct function
+
+/**
+ *
+ * @param {str} name
+ * @param {number} age
+ */
+function User(name = "kissa", age = 30) {
+    this.name = name;
+    this.age = age;
+
+    return { name: this.name, age: this.age };
 }
 
-person.sayHi()
+let usr = new User("bersy", 52);
+console.log(usr);
